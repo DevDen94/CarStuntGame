fa749de603a00d25954ef042ec49547a9d20f544
DevRida <129722785+RidaDev12@users.noreply.github.com>
2023-08-08 19:46:12 +0500

    Final Texture Change

--- Assets/Scripts/HomeManager.cs
+++ Assets/Scripts/HomeManager.cs
@@ -12,10 +12,10 @@ public class HomeManager : MonoBehaviour
     private void Start()
     {
 
-        //PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 1), 0);
-        //PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 2), 0);
-        //PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 3), 0);
-        //PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 4), 0);
+        ////PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 1), 0);
+        ////PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 2), 0);
+        ////PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 3), 0);
+        ////PlayerPrefs.SetInt("CatLock_" + (HomeManager._currentCategory + 4), 0);
 
 
         Time.timeScale = 1;
